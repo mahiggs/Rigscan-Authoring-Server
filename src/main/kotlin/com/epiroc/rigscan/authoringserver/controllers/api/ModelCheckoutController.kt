c07704d31932c2903cc6e970a553a6f65e966787
Thomas Joiner <thomas.b.joiner@gmail.com>
2018-10-05 14:28:54 -0500

    add log message

--- src/main/kotlin/com/epiroc/rigscan/authoringserver/controllers/api/ModelCheckoutController.kt
+++ src/main/kotlin/com/epiroc/rigscan/authoringserver/controllers/api/ModelCheckoutController.kt
@@ -3,6 +3,8 @@ package com.epiroc.rigscan.authoringserver.controllers.api
 import com.epiroc.rigscan.authoringserver.db.isAdministrator
 import com.epiroc.rigscan.authoringserver.db.repositories.UserRepository
 import com.epiroc.rigscan.authoringserver.db.repositories.currentUser
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.jdbc.core.JdbcTemplate
@@ -17,6 +19,7 @@ import java.util.*
 class ModelCheckoutController(private val template: JdbcTemplate, private val repository: UserRepository) {
 
     companion object {
+        private val log: Logger = LoggerFactory.getLogger(ModelCheckoutController::class.java)
         const val CHECK_OUT_PROTOCOL_DML: String = "INSERT INTO audit_protocol_checkouts (model_id, checked_out_by, checkout_performed_by, checked_out_reason) VALUES (?, ?, ?, ?)"
         const val CHECK_IN_PROTOCOL_DML: String = "DELETE FROM audit_protocol_checkouts WHERE model_id=?"
     }
@@ -57,6 +60,8 @@ class ModelCheckoutController(private val template: JdbcTemplate, private val re
     @DeleteMapping("/{modelId}")
     fun checkModelIn(@PathVariable modelId: UUID) : ResponseEntity<Any> {
         val currentUser = this.repository.currentUser()
+
+        log.info("Received checkin request from ${currentUser.userName} [id=${currentUser.id}] for model [id=$modelId]")
 
         val currentCheckoutStatus = this.template.queryForList("SELECT checked_out_by FROM audit_protocol_checkouts WHERE model_id=?",
                 arrayOf(modelId.toString()), intArrayOf(Types.CHAR), Integer::class.java)
