c712cfa1f918adb4f9543dd282d121c49fc49d3b
Thomas Joiner <thomas.b.joiner@gmail.com>
2018-10-05 12:39:38 -0500

    add checks to safety file upload to ensure that the specified product company exists

--- src/main/kotlin/com/epiroc/rigscan/authoringserver/controllers/api/FilesController.kt
+++ src/main/kotlin/com/epiroc/rigscan/authoringserver/controllers/api/FilesController.kt
@@ -8,12 +8,9 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.RowMapper
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import java.io.File
 import java.nio.file.Paths
-import java.sql.ResultSet
 import java.sql.Types
 import java.util.*
 
@@ -27,9 +24,13 @@ class FilesController(val properties: RigscanProperties, val template: JdbcTempl
     }
 
     @PostMapping("/safety/{productCompany}")
-    fun uploadSafetyImage(@PathVariable productCompany: String, @RequestParam("file") file: MultipartFile) : ResponseEntity<Any> {
+    fun uploadSafetyImage(@PathVariable productCompany: String, @RequestParam("file") file: MultipartFile): ResponseEntity<Any> {
         val cleanFilename = file.originalFilename.cleanFilename()
         log.info("Uploading safety file, file name: {} [cleaned={}]", file.originalFilename, cleanFilename)
+
+        if (!Paths.get(properties.files.directory, productCompany).toFile().exists()) {
+            return ResponseEntity.badRequest().body(Message("Unrecognized product company: $productCompany"))
+        }
 
         // safety files are stored in a directory with the format ${baseDir}/${productCompany}/assets/safety/${fileName}
         val path = Paths.get(properties.files.directory, productCompany, "assets", "safety", cleanFilename)
@@ -41,7 +42,7 @@ class FilesController(val properties: RigscanProperties, val template: JdbcTempl
     }
 
     @PostMapping("/{modelId}")
-    fun createFile(@PathVariable modelId: UUID, @RequestParam("file") file: MultipartFile) : ResponseEntity<Any> {
+    fun createFile(@PathVariable modelId: UUID, @RequestParam("file") file: MultipartFile): ResponseEntity<Any> {
         val currentUser = this.repository.currentUser()
 
         // we will always allow administrators to upload files
@@ -54,7 +55,7 @@ class FilesController(val properties: RigscanProperties, val template: JdbcTempl
             }
         }
 
-        val modelInfo = this.template.query(MODEL_INFORMATION, arrayOf(modelId.toString()), intArrayOf(Types.CHAR)) { rs, rowNum ->
+        val modelInfo = this.template.query(MODEL_INFORMATION, arrayOf(modelId.toString()), intArrayOf(Types.CHAR)) { rs, _ ->
             ModelInformation(rs.getNString("model_id"),
                     rs.getNString("product_company"), rs.getBoolean("template"))
         }
@@ -96,7 +97,7 @@ class FilesController(val properties: RigscanProperties, val template: JdbcTempl
  * Whitespace characters
  * ```
  */
-fun String?.cleanFilename() : String? {
+fun String?.cleanFilename(): String? {
     return this?.replace(Regex("\\s+"), "_")
             ?.replace(Regex("[\\x00-\\x1F/<>:\"\\\\\\|?*]*"), "")
 }
